21316e4da41622af6e370b9fdeab6cfed0caa442
Chrilleweb <christian.munknissen@gmail.com>
2026-03-02 09:37:47 +0100

    chore: workflows and eslint

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,18 @@
+<!-- Your PR description here -->
+
+---
+
+### Please don't delete this checklist! Before submitting the PR, please make sure you do the following:
+
+- [ ] The changes are small, focused, and easy to review
+- [ ] An image or GIF is included if the PR contains UI changes
+- [ ] The existing code style and project structure are followed
+- [ ] Link to related issues (if applicable)
+
+### Tests
+
+- [ ] Relevant unit and/or e2e tests have been added or updated
+
+---
+
+See [CONTRIBUTING.md](https://github.com/Chrilleweb/licop/blob/main/CONTRIBUTING.md)
---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      quotes: ['warn', 'single', { avoidEscape: true }],
+    },
+  },
+  {
+    ignores: ['dist/**'],
+  },
+];
